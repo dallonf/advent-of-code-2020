ec52d2398563a51835ed3ea60018ce86b1bfc485
Dallon Feldner <dallon.feldner@gmail.com>
2020-12-03 07:16:26 -0600

    Day 3 Part One

--- shared/src/puzzle_input.rs
+++ shared/src/puzzle_input.rs
@@ -5,3 +5,7 @@ pub fn lines(puzzle_input: &str) -> Vec<String> {
         .map(|line| String::from(line))
         .collect()
 }
+
+pub fn to_strs<'a>(input: &'a Vec<String>) -> Vec<&'a str> {
+    return input.as_slice().iter().map(|x| x.as_str()).collect();
+}
