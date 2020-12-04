ca5825f180ff3b4dc412b9697b880b84d9a1174e
Dallon Feldner <dallon.feldner@volusion.com>
2020-12-04 12:55:52 -0600

    Day 4 Part One

--- puzzles/day04/src/lib.rs
+++ puzzles/day04/src/lib.rs
@@ -1,9 +1,45 @@
 // Day 4: Passport Processing
 
 use shared::prelude::*;
+use std::collections::HashMap;
+
+type PassportRecord = HashMap<String, String>;
 
 lazy_static! {
-    static ref PUZZLE_INPUT: Vec<String> = puzzle_input::lines(include_str!("puzzle_input.txt"));
+    static ref PUZZLE_INPUT: Vec<PassportRecord> = parse_input(&puzzle_input::to_strs(
+        &puzzle_input::lines(include_str!("puzzle_input.txt"))
+    ));
+    static ref TEST_INPUT: Vec<PassportRecord> = parse_input(&puzzle_input::to_strs(
+        &puzzle_input::lines(include_str!("test_input.txt"))
+    ));
+    static ref REQUIRED_ENTRIES: Vec<&'static str> =
+        vec!["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
+}
+
+pub fn parse_input(input_lines: &[&str]) -> Vec<PassportRecord> {
+    input_lines
+        .split(|x| x.trim().is_empty())
+        .map(|definition_lines| {
+            definition_lines
+                .join(" ")
+                .split(" ")
+                .map(|field| {
+                    let pair: Vec<_> = field.split(":").collect();
+                    (pair[0].to_string(), pair[1].to_string())
+                })
+                .collect()
+        })
+        .collect()
+}
+
+pub fn is_valid(passport: &PassportRecord) -> bool {
+    REQUIRED_ENTRIES
+        .iter()
+        .all(|x| passport.contains_key(&x.to_string()))
+}
+
+pub fn valid_passports(passports: &[PassportRecord]) -> usize {
+    passports.iter().filter(|x| is_valid(x)).count()
 }
 
 #[cfg(test)]
@@ -12,12 +48,14 @@ mod part_one {
 
     #[test]
     fn test_cases() {
-        assert_eq!(1 + 1, 2);
+        let result: Vec<bool> = TEST_INPUT.iter().map(|x| is_valid(x)).collect();
+        assert_eq!(result, vec![true, false, true, false]);
+        assert_eq!(valid_passports(&TEST_INPUT), 2);
     }
 
     #[test]
     fn answer() {
-        assert_eq!(*PUZZLE_INPUT, Vec::<String>::new());
+        assert_eq!(valid_passports(&PUZZLE_INPUT), 202);
     }
 }
 
