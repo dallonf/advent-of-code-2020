944e318352a66da34e572b2764db7bbad619cc2e
Dallon Feldner <dallon.feldner@gmail.com>
2020-12-09 07:42:45 -0600

    Day 9 Part One

---
+++ puzzles/day09/src/lib.rs
@@ -0,0 +1,96 @@
+// Day 9: Encoding Error
+
+use std::str::FromStr;
+
+use shared::prelude::*;
+
+#[derive(Debug)]
+pub struct XmasStream<'a> {
+    prev: &'a [i64],
+    next: &'a i64,
+}
+
+#[derive(Debug)]
+pub enum XmasStreamResult {
+    Continue,
+    Invalid(i64),
+}
+
+lazy_static! {
+    static ref PUZZLE_INPUT: Vec<i64> = puzzle_input::lines(include_str!("puzzle_input.txt"))
+        .into_iter()
+        .map(FromStr::from_str)
+        .collect::<Result<_, _>>()
+        .unwrap();
+    static ref TEST_INPUT: Vec<i64> = vec![
+        35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309, 576
+    ];
+}
+
+impl XmasStream<'_> {
+    fn result(&self) -> XmasStreamResult {
+        let mut possible_matches =
+            self.prev
+                .iter()
+                .enumerate()
+                .flat_map(|(i, x)| -> Box<dyn Iterator<Item = i64>> {
+                    if i + 1 >= self.prev.len() {
+                        return Box::new(std::iter::empty());
+                    }
+                    let others = &self.prev[i + 1..];
+                    Box::new(others.iter().map(move |y| x + y))
+                });
+
+        if possible_matches.any(|x| &x == self.next) {
+            XmasStreamResult::Continue
+        } else {
+            XmasStreamResult::Invalid(*self.next)
+        }
+    }
+}
+
+pub fn find_first_invalid_number(stream: &[i64], preamble: usize) -> Option<i64> {
+    let stream_points = stream
+        .iter()
+        .enumerate()
+        .skip(preamble)
+        .map(|(i, x)| XmasStream {
+            prev: &stream[i - preamble..i],
+            next: x,
+        });
+
+    stream_points.map(|x| x.result()).find_map(|x| match x {
+        XmasStreamResult::Invalid(x) => Some(x),
+        _ => None,
+    })
+}
+
+#[cfg(test)]
+mod part_one {
+    use super::*;
+
+    #[test]
+    fn test_cases() {
+        assert_eq!(
+            find_first_invalid_number(TEST_INPUT.as_slice(), 5).unwrap(),
+            127
+        );
+    }
+
+    #[test]
+    fn answer() {
+        assert_eq!(
+            find_first_invalid_number(PUZZLE_INPUT.as_slice(), 25).unwrap(),
+            1212510616
+        );
+    }
+}
+
+// #[cfg(test)]
+// mod part_two {
+//     use super::*;
+//     #[test]
+//     fn test_cases() {}
+//     #[test]
+//     fn answer() {}
+// }
