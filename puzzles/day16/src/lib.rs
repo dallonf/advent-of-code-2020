4f0b4442b3fee51aeac0b159d8feb02936669cd8
Dallon Feldner <dallon.feldner@volusion.com>
2020-12-16 15:31:28 -0600

    Major updates (misc)

--- puzzles/day16/src/lib.rs
+++ puzzles/day16/src/lib.rs
@@ -1,12 +1,18 @@
 // Day 00: Template
 
-use std::{collections::HashMap, ops::Range};
+use std::{
+    collections::{HashMap, HashSet},
+    ops::Range,
+};
 
 use shared::prelude::*;
 
 pub type Rules = HashMap<String, Vec<Range<u32>>>;
 
 pub type Ticket = Vec<u32>;
+
+#[derive(Default, Debug, PartialEq, Eq)]
+pub struct FieldMapping(Vec<String>);
 
 #[derive(Default, Debug, PartialEq, Eq)]
 pub struct ProblemNotes {
@@ -20,6 +26,8 @@ lazy_static! {
         Regex::new(r"^([a-z ]+): ([0-9]+)\-([0-9]+) or ([0-9]+)\-([0-9]+)$").unwrap();
     static ref TEST_INPUT: ProblemNotes =
         ProblemNotes::parse_input(&puzzle_input::lines(include_str!("test_input.txt"))).unwrap();
+    static ref TEST_INPUT_2: ProblemNotes =
+        ProblemNotes::parse_input(&puzzle_input::lines(include_str!("test_input_2.txt"))).unwrap();
     static ref PUZZLE_INPUT: ProblemNotes =
         ProblemNotes::parse_input(&puzzle_input::lines(include_str!("puzzle_input.txt"))).unwrap();
 }
@@ -99,6 +107,162 @@ impl ProblemNotes {
             .filter(|x| !all_ranges.iter().any(|range| range.contains(x)))
             .sum()
     }
+
+    pub fn field_mapping(&self) -> FieldMapping {
+        let all_valid_tickets: Vec<&Ticket> = {
+            let all_ranges: Vec<&Range<u32>> = self.rules.values().flatten().collect();
+
+            let not_invalid = |ticket: &&Ticket| {
+                ticket
+                    .iter()
+                    .all(|x| all_ranges.iter().any(|range| range.contains(x)))
+            };
+
+            self.nearby_tickets
+                .iter()
+                .filter(not_invalid)
+                .chain(std::iter::once(&self.your_ticket))
+                .collect()
+        };
+
+        let columns: Vec<Vec<u32>> = {
+            let num_columns = self.rules.len();
+
+            let all_values_for_column =
+                |i| all_valid_tickets.iter().map(|ticket| ticket[i]).collect();
+
+            (0..num_columns).map(all_values_for_column).collect()
+        };
+
+        let possibilities_for_columns: Vec<HashSet<&String>> = {
+            let all_possibilities: HashSet<&String> = self.rules.keys().collect();
+
+            let possibilities_for_column = |column: &Vec<u32>| {
+                column.iter().fold(all_possibilities.clone(), |prev, num| {
+                    let matches_rule = |key: &&String| {
+                        self.rules
+                            .get(key.to_owned())
+                            .unwrap()
+                            .iter()
+                            .any(|range| range.contains(num))
+                    };
+
+                    prev.into_iter().filter(matches_rule).collect()
+                })
+            };
+
+            columns.iter().map(possibilities_for_column).collect()
+        };
+
+        #[derive(Debug, PartialEq, Eq, Clone)]
+        enum ColumnState<'a> {
+            Solved(&'a String),
+            Possibilities(HashSet<&'a String>),
+        }
+
+        fn solve_columns(possibilities: Vec<ColumnState>) -> FieldMapping {
+            // All are solved; stop recursing
+            if possibilities.iter().all(|x| {
+                if let ColumnState::Solved(_) = x {
+                    true
+                } else {
+                    false
+                }
+            }) {
+                return FieldMapping(
+                    possibilities
+                        .into_iter()
+                        .map(|x| {
+                            if let ColumnState::Solved(x) = x {
+                                x.to_owned()
+                            } else {
+                                panic!()
+                            }
+                        })
+                        .collect(),
+                );
+            }
+
+            let all_solved: HashSet<&String> = possibilities
+                .iter()
+                .filter_map(|x| {
+                    if let ColumnState::Solved(x) = x {
+                        Some(x)
+                    } else {
+                        None
+                    }
+                })
+                .copied()
+                .collect();
+
+            let iterate_column_possibilities =
+                |i: usize, column_possibilities: &HashSet<&String>| -> ColumnState {
+                    let column_possibilities: HashSet<&String> = column_possibilities
+                        .difference(&all_solved)
+                        .copied()
+                        .collect();
+
+                    let all_other_possibilities: HashSet<&String> = possibilities
+                        .iter()
+                        .enumerate()
+                        .map(|(j, x)| {
+                            if j == i {
+                                return None;
+                            }
+                            if let ColumnState::Possibilities(x) = x {
+                                Some(x)
+                            } else {
+                                None
+                            }
+                        })
+                        .flatten()
+                        .fold(HashSet::new(), |a, b| a.union(b).copied().collect());
+
+                    let unique_column_possibilities: HashSet<&String> = column_possibilities
+                        .difference(&all_other_possibilities)
+                        .copied()
+                        .collect();
+
+                    if unique_column_possibilities.len() == 1 {
+                        ColumnState::Solved(unique_column_possibilities.iter().next().unwrap())
+                    } else {
+                        ColumnState::Possibilities(column_possibilities)
+                    }
+                };
+
+            let possibilities_after_solving: Vec<ColumnState> = {
+                possibilities
+                    .iter()
+                    .enumerate()
+                    .map(|(i, x)| match x {
+                        ColumnState::Solved(x) => ColumnState::Solved(x),
+                        ColumnState::Possibilities(possibilities) => {
+                            iterate_column_possibilities(i, possibilities)
+                        }
+                    })
+                    .collect()
+            };
+
+            if possibilities_after_solving == possibilities {
+                panic!("No solutions found! Got stuck on: {:#?}", possibilities);
+            }
+
+            todo!()
+        }
+
+        solve_columns(
+            possibilities_for_columns
+                .into_iter()
+                .map(|x| ColumnState::Possibilities(x))
+                .collect(),
+        )
+    }
+}
+
+impl FieldMapping {
+    pub fn translate(&self, ticket: &Ticket) -> HashMap<String, u32> {
+        todo!()
+    }
 }
 
 #[cfg(test)]
@@ -123,11 +287,27 @@ mod part_one {
     }
 }
 
-// #[cfg(test)]
-// mod part_two {
-//     use super::*;
-//     #[test]
-//     fn test_cases() {}
-//     #[test]
-//     fn answer() {}
-// }
+#[cfg(test)]
+mod part_two {
+    use super::*;
+    #[test]
+    fn test_case() {
+        let mapping = TEST_INPUT_2.field_mapping();
+
+        println!("{:?}", mapping);
+
+        let mapped_ticket = mapping.translate(&TEST_INPUT_2.your_ticket);
+
+        let expected: HashMap<String, u32> = vec![
+            ("class".to_string(), 12),
+            ("row".to_string(), 11),
+            ("seat".to_string(), 13),
+        ]
+        .into_iter()
+        .collect();
+
+        assert_eq!(mapped_ticket, expected);
+    }
+    // #[test]
+    // fn answer() {}
+}
