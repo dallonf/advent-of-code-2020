c836d139ab61f08d3ee62c98d37717404f623a44
Dallon Feldner <dallon.feldner@gmail.com>
2020-12-18 08:22:20 -0600

    Day 18 Part One

--- puzzles/day18/src/lib.rs
+++ puzzles/day18/src/lib.rs
@@ -5,7 +5,7 @@ use std::{convert::TryFrom, iter::Peekable};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Token {
-    Number(i32),
+    Number(i64),
     ParenOpen,
     ParenClose,
     Operator(Operator),
@@ -18,8 +18,8 @@ pub enum Operator {
 }
 
 lazy_static! {
-    // static ref PUZZLE_INPUT: Vec<&'static str> =
-    //     puzzle_input::lines(include_str!("puzzle_input.txt"));
+    static ref PUZZLE_INPUT: Vec<&'static str> =
+        puzzle_input::lines(include_str!("puzzle_input.txt"));
 }
 
 pub fn parse(s: &str) -> impl Iterator<Item = anyhow::Result<Token>> + '_ {
@@ -31,40 +31,41 @@ pub fn parse(s: &str) -> impl Iterator<Item = anyhow::Result<Token>> + '_ {
         x => x
             .to_digit(10)
             .ok_or(anyhow!("Unsupported character"))
-            .and_then(|x| i32::try_from(x).map_err(|e| anyhow::Error::from(e)))
+            .and_then(|x| i64::try_from(x).map_err(|e| anyhow::Error::from(e)))
             .map(|x| Token::Number(x)),
     })
 }
 
 pub fn evaluate_expression(
-    mut input: Peekable<impl Iterator<Item = anyhow::Result<Token>>>,
-) -> anyhow::Result<i32> {
-    let first_number = input.next().ok_or(anyhow!("Empty expression"))??;
-    let first_number = if let Token::Number(x) = first_number {
-        x
-    } else {
-        return Err(anyhow!("Expected number"));
-    };
-
-    continue_operation(first_number, &mut input)
+    input: &mut Peekable<impl Iterator<Item = anyhow::Result<Token>>>,
+) -> anyhow::Result<i64> {
+    let first_number = evaluate_numeric_value(input)?;
+    continue_operation(first_number, input)
 }
 
-pub fn continue_operation(
-    lhs: i32,
+fn evaluate_numeric_value(
     input: &mut Peekable<impl Iterator<Item = anyhow::Result<Token>>>,
-) -> anyhow::Result<i32> {
+) -> anyhow::Result<i64> {
+    match input.next() {
+        Some(Ok(Token::Number(number))) => Ok(number),
+        Some(Ok(Token::ParenOpen)) => evaluate_expression(input),
+        Some(Err(err)) => Err(err),
+        Some(_) | None => Err(anyhow!("Expected number")),
+    }
+}
+
+fn continue_operation(
+    lhs: i64,
+    input: &mut Peekable<impl Iterator<Item = anyhow::Result<Token>>>,
+) -> anyhow::Result<i64> {
     let operator = match input.next() {
         Some(Ok(Token::Operator(x))) => Ok(x),
+        None | Some(Ok(Token::ParenClose)) => return Ok(lhs),
         Some(Ok(_)) => Err(anyhow!("Expected an operator")),
         Some(Err(err)) => Err(err),
-        None => return Ok(lhs),
     }?;
 
-    let rhs = match input.next() {
-        Some(Ok(Token::Number(x))) => Ok(x),
-        Some(Ok(_)) | None => Err(anyhow!("Expected a number")),
-        Some(Err(err)) => Err(err),
-    }?;
+    let rhs = evaluate_numeric_value(input)?;
 
     let result = match operator {
         Operator::Add => lhs + rhs,
@@ -74,22 +75,51 @@ pub fn continue_operation(
     continue_operation(result, input)
 }
 
+pub fn eval(s: &str) -> anyhow::Result<i64> {
+    evaluate_expression(&mut parse(s).peekable())
+}
+
 #[cfg(test)]
 mod part_one {
     use super::*;
 
     #[test]
     fn test_case_1() {
+        assert_eq!(eval("1 + 2 * 3 + 4 * 5 + 6").unwrap(), 71);
+    }
+
+    #[test]
+    fn test_parentheses() {
+        assert_eq!(eval("1 + (2 * 3) + (4 * (5 + 6))").unwrap(), 51);
+    }
+
+    #[test]
+    fn more_test_cases() {
+        assert_eq!(eval("2 * 3 + (4 * 5)").unwrap(), 26);
+        assert_eq!(eval("5 + (8 * 3 + 9 + 3 * 4 * 3)").unwrap(), 437);
         assert_eq!(
-            evaluate_expression(parse("1 + 2 * 3 + 4 * 5 + 6").peekable()).unwrap(),
-            71
+            eval("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))").unwrap(),
+            12240
+        );
+        assert_eq!(
+            eval("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2").unwrap(),
+            13632
         );
     }
 
-    // #[test]
-    // fn answer() {
-    //     assert_eq!(*PUZZLE_INPUT, Vec::<String>::new());
-    // }
+    #[test]
+    fn answer() {
+        let result = PUZZLE_INPUT
+            .iter()
+            .map(|&x| eval(x))
+            .try_fold(0, |a, b| match b {
+                Ok(b) => Ok(a + b),
+                err @ Err(_) => err,
+            })
+            .unwrap();
+
+        assert_eq!(result, 11004703763391);
+    }
 }
 
 // #[cfg(test)]
