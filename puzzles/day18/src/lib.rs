c8aa1d039c89dcbdfa8bbb213ef2894646d73b8e
Dallon Feldner <dallon.feldner@volusion.com>
2020-12-18 17:44:20 -0600

    Day 18 Part One refactor

--- puzzles/day18/src/lib.rs
+++ puzzles/day18/src/lib.rs
@@ -1,7 +1,7 @@
 // Day 18: Operation Order
 
 use shared::prelude::*;
-use std::{convert::TryFrom, iter::Peekable};
+use std::convert::TryFrom;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Token {
@@ -17,66 +17,120 @@ pub enum Operator {
     Multiply,
 }
 
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum Expression {
+    Number(i64),
+    Operation(Operation),
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Operation {
+    left: Box<Expression>,
+    right: Vec<OperationContinuation>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct OperationContinuation(Operator, Box<Expression>);
+
 lazy_static! {
     static ref PUZZLE_INPUT: Vec<&'static str> =
         puzzle_input::lines(include_str!("puzzle_input.txt"));
 }
 
-pub fn parse(s: &str) -> impl Iterator<Item = anyhow::Result<Token>> + '_ {
-    s.chars().filter(|&x| x != ' ').map(|x| match x {
-        '(' => Ok(Token::ParenOpen),
-        ')' => Ok(Token::ParenClose),
-        '+' => Ok(Token::Operator(Operator::Add)),
-        '*' => Ok(Token::Operator(Operator::Multiply)),
-        x => x
-            .to_digit(10)
-            .ok_or(anyhow!("Unsupported character"))
-            .and_then(|x| i64::try_from(x).map_err(|e| anyhow::Error::from(e)))
-            .map(|x| Token::Number(x)),
-    })
+pub fn parse(s: &str) -> anyhow::Result<Vec<Token>> {
+    s.chars()
+        .filter(|&x| x != ' ')
+        .map(|x| match x {
+            '(' => Ok(Token::ParenOpen),
+            ')' => Ok(Token::ParenClose),
+            '+' => Ok(Token::Operator(Operator::Add)),
+            '*' => Ok(Token::Operator(Operator::Multiply)),
+            x => x
+                .to_digit(10)
+                .ok_or(anyhow!("Unsupported character"))
+                .and_then(|x| i64::try_from(x).map_err(|e| anyhow::Error::from(e)))
+                .map(|x| Token::Number(x)),
+        })
+        .collect()
 }
 
-pub fn evaluate_expression(
-    input: &mut Peekable<impl Iterator<Item = anyhow::Result<Token>>>,
-) -> anyhow::Result<i64> {
-    let first_number = evaluate_numeric_value(input)?;
-    continue_operation(first_number, input)
-}
+impl Expression {
+    pub fn parse(input: &mut impl Iterator<Item = Token>) -> anyhow::Result<Expression> {
+        Ok(Expression::Operation(Operation::try_collect(input)?))
+    }
 
-fn evaluate_numeric_value(
-    input: &mut Peekable<impl Iterator<Item = anyhow::Result<Token>>>,
-) -> anyhow::Result<i64> {
-    match input.next() {
-        Some(Ok(Token::Number(number))) => Ok(number),
-        Some(Ok(Token::ParenOpen)) => evaluate_expression(input),
-        Some(Err(err)) => Err(err),
-        Some(_) | None => Err(anyhow!("Expected number")),
+    fn parse_numeric(input: &mut impl Iterator<Item = Token>) -> anyhow::Result<Expression> {
+        match input.next() {
+            Some(Token::Number(number)) => Ok(Expression::Number(number)),
+            Some(Token::ParenOpen) => Expression::parse(input),
+            Some(_) | None => Err(anyhow!("Expected number")),
+        }
+    }
+
+    fn evaluate(&self) -> i64 {
+        match self {
+            Expression::Number(num) => *num,
+            Expression::Operation(operation) => operation.evaluate(),
+        }
     }
 }
 
-fn continue_operation(
-    lhs: i64,
-    input: &mut Peekable<impl Iterator<Item = anyhow::Result<Token>>>,
-) -> anyhow::Result<i64> {
-    let operator = match input.next() {
-        Some(Ok(Token::Operator(x))) => Ok(x),
-        None | Some(Ok(Token::ParenClose)) => return Ok(lhs),
-        Some(Ok(_)) => Err(anyhow!("Expected an operator")),
-        Some(Err(err)) => Err(err),
-    }?;
+impl Operation {
+    pub fn try_collect(input: &mut impl Iterator<Item = Token>) -> anyhow::Result<Operation> {
+        let left = Box::new(Expression::parse_numeric(input)?);
 
-    let rhs = evaluate_numeric_value(input)?;
+        fn next(
+            iter: &mut impl Iterator<Item = Token>,
+            mut list_so_far: Vec<OperationContinuation>,
+        ) -> anyhow::Result<Vec<OperationContinuation>> {
+            let operator = iter.next();
+            match operator {
+                Some(Token::Operator(operator)) => {
+                    let expression = Expression::parse_numeric(iter)?;
+                    let continuation = OperationContinuation(operator, Box::new(expression));
+                    list_so_far.push(continuation);
+                    next(iter, list_so_far)
+                }
+                None | Some(Token::ParenClose) => Ok(list_so_far), // end of operation
+                Some(_) => Err(anyhow!("Expected an operator")),
+            }
+        }
 
-    let result = match operator {
-        Operator::Add => lhs + rhs,
-        Operator::Multiply => lhs * rhs,
-    };
+        let right = next(input, vec![])?;
 
-    continue_operation(result, input)
+        Ok(Operation { left, right })
+    }
+
+    fn evaluate(&self) -> i64 {
+        let left = self.left.deref().evaluate();
+        if self.right.is_empty() {
+            return left;
+        }
+
+        // Solve first continuation
+        let (OperationContinuation(operation, expression), new_right) =
+            self.right.split_first().unwrap();
+        let expression = expression.evaluate();
+
+        let new_left = match operation {
+            Operator::Add => left + expression,
+            Operator::Multiply => left * expression,
+        };
+
+        let new_operation = Operation {
+            left: Box::new(Expression::Number(new_left)),
+            right: new_right.to_vec(),
+        };
+
+        new_operation.evaluate()
+    }
 }
 
 pub fn eval(s: &str) -> anyhow::Result<i64> {
-    evaluate_expression(&mut parse(s).peekable())
+    let tokens = parse(s)?;
+    let expression: Expression = Expression::parse(&mut tokens.into_iter())?;
+
+    Ok(expression.evaluate())
 }
 
 #[cfg(test)]
