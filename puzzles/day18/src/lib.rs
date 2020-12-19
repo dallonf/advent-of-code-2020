9ae8bc4e5a447461ae89498fae55da3fb3f099b1
Dallon Feldner <dallon.feldner@gmail.com>
2020-12-18 18:40:47 -0600

    Day 18 Part Two

--- puzzles/day18/src/lib.rs
+++ puzzles/day18/src/lib.rs
@@ -1,7 +1,8 @@
 // Day 18: Operation Order
 
+use std::fmt::{Debug, Display};
+
 use shared::prelude::*;
-use std::convert::TryFrom;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Token {
@@ -72,6 +73,13 @@ impl Expression {
             Expression::Operation(operation) => operation.evaluate(),
         }
     }
+
+    fn evaluate_mk2(&self) -> i64 {
+        match self {
+            Expression::Number(num) => *num,
+            Expression::Operation(operation) => operation.evaluate_mk2(),
+        }
+    }
 }
 
 impl Operation {
@@ -101,7 +109,7 @@ impl Operation {
     }
 
     fn evaluate(&self) -> i64 {
-        let left = self.left.deref().evaluate();
+        let left = self.left.evaluate();
         if self.right.is_empty() {
             return left;
         }
@@ -123,6 +131,101 @@ impl Operation {
 
         new_operation.evaluate()
     }
+
+    fn evaluate_mk2(&self) -> i64 {
+        if self.right.is_empty() {
+            return self.left.evaluate_mk2();
+        }
+
+        let continuation_to_eval = self
+            .right
+            .iter()
+            .enumerate()
+            // first try to find the first add operator
+            .find(|(_, OperationContinuation(op, _))| *op == Operator::Add)
+            // then settle for the first operator available
+            .or_else(|| self.right.first().map(|first| (0, first)))
+            .unwrap();
+        let (index, OperationContinuation(operator, expression)) = continuation_to_eval;
+
+        let rhs = expression.evaluate_mk2();
+        let lhs = if index == 0 {
+            self.left.evaluate_mk2()
+        } else {
+            self.right[index - 1].1.evaluate_mk2()
+        };
+
+        let new_value = match operator {
+            Operator::Add => lhs + rhs,
+            Operator::Multiply => lhs * rhs,
+        };
+
+        // Collapse the new value into the previous container
+        let new_operation = {
+            if index == 0 {
+                let (_, new_right) = self.right.split_first().unwrap();
+                Operation {
+                    left: Box::new(Expression::Number(new_value)),
+                    right: new_right.to_vec(),
+                }
+            } else {
+                let new_right = self
+                    .right
+                    .iter()
+                    .enumerate()
+                    // Remove the evaluated operation
+                    .filter(|(i, _)| *i != index)
+                    .map(|(i, continuation)| {
+                        if i == index - 1 {
+                            OperationContinuation(
+                                continuation.0,
+                                Box::new(Expression::Number(new_value)),
+                            )
+                        } else {
+                            continuation.to_owned()
+                        }
+                    })
+                    .collect();
+
+                Operation {
+                    left: self.left.to_owned(),
+                    right: new_right,
+                }
+            }
+        };
+
+        new_operation.evaluate_mk2()
+    }
+}
+
+impl Display for Expression {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Expression::Number(num) => f.write_fmt(format_args!("{}", num)),
+            Expression::Operation(operation) => f.write_fmt(format_args!("({})", operation)),
+        }
+    }
+}
+
+impl Display for Operation {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let tokens = self
+            .right
+            .iter()
+            .flat_map(|OperationContinuation(operator, expression)| {
+                vec![
+                    match operator {
+                        Operator::Add => "+".to_string(),
+                        Operator::Multiply => "*".to_string(),
+                    },
+                    expression.to_string(),
+                ]
+            })
+            .collect::<Vec<String>>()
+            .join(" ");
+
+        f.write_str(&format!("{} {}", self.left.to_string(), tokens))
+    }
 }
 
 pub fn eval(s: &str) -> anyhow::Result<i64> {
@@ -130,6 +233,13 @@ pub fn eval(s: &str) -> anyhow::Result<i64> {
     let expression: Expression = Expression::parse(&mut tokens.into_iter())?;
 
     Ok(expression.evaluate())
+}
+
+pub fn eval_mk2(s: &str) -> anyhow::Result<i64> {
+    let tokens = parse(s)?;
+    let expression: Expression = Expression::parse(&mut tokens.into_iter())?;
+
+    Ok(expression.evaluate_mk2())
 }
 
 #[cfg(test)]
@@ -175,11 +285,40 @@ mod part_one {
     }
 }
 
-// #[cfg(test)]
-// mod part_two {
-//     use super::*;
-//     #[test]
-//     fn test_cases() {}
-//     #[test]
-//     fn answer() {}
-// }
+#[cfg(test)]
+mod part_two {
+    use super::*;
+    #[test]
+    fn test_case_1() {
+        assert_eq!(eval_mk2("1 + 2 * 3 + 4 * 5 + 6").unwrap(), 231);
+    }
+
+    #[test]
+    fn test_cases() {
+        assert_eq!(eval_mk2("1 + (2 * 3) + (4 * (5 + 6))").unwrap(), 51);
+        assert_eq!(eval_mk2("2 * 3 + (4 * 5)").unwrap(), 46);
+        assert_eq!(eval_mk2("5 + (8 * 3 + 9 + 3 * 4 * 3)").unwrap(), 1445);
+        assert_eq!(
+            eval_mk2("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))").unwrap(),
+            669060
+        );
+        assert_eq!(
+            eval_mk2("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2").unwrap(),
+            23340
+        );
+    }
+
+    #[test]
+    fn answer() {
+        let result = PUZZLE_INPUT
+            .iter()
+            .map(|&x| eval_mk2(x))
+            .try_fold(0, |a, b| match b {
+                Ok(b) => Ok(a + b),
+                err @ Err(_) => err,
+            })
+            .unwrap();
+
+        assert_eq!(result, 290726428573651);
+    }
+}
